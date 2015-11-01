24f07779ce3532120c38b0ddd39bc2c73c4410da
Erika Harvey <erika@erikasf.com>
2015-10-31 17:49:20 -0700

    woot, checkout my nifty logo animation on the home page

--- scripts/collection.js
+++ scripts/collection.js
@@ -1,7 +1,6 @@
 
  var buildCollectionItemTemplate = function() {
- 
-     var template =
+ var template =
      '<div class="collection-album-container column fourth">'
    + '  <img src="assets/images/album_covers/01.png"/>'
    + '  <div class="collection-album-info caption">'
--- scripts/landing.js
+++ scripts/landing.js
@@ -1,33 +1,37 @@
 
 
-
- var animatePoints = function() {
-     //Animates points           
-    var revealPoint = function() {
+var animateLogo = function(){
+    var revealLogo = function(){
         $(this).css({
-             opacity: 1,
-             transform: 'scaleX(1) translateY(0)'
-         });
+            opacity: 1
+        })
+    }
+}
+ var animatePoints = (function() {
+   var revealPoint = function() {
+      $(this).css({
+          opacity: 1,
+          transform: 'scaleX(1) translateY(0)'
+  });   
      };
     $.each($('.point'), revealPoint);
-};
- 
+});
 
  $(window).load(function() {
-
-    var pointsArray = document.getElementsByClassName('point');
-
-    // Automatically animates the points on a tall screen where scrolling can't trigger the animation
-      if ($(window).height() > 950) {
-         animatePoints();
+    if ($(window).height() > 950) {
+       animatePoints();
      }
+
+    // var pointsArray = document.getElementsByClassName('point');
+
+    var scrollDistance = $('.selling-points').offset().top - $(window).height() + 200;
+     
 
     //Automatically animates points once the screen scrolls to their position
       $(window).scroll(function(event) {
-
-            if ($(window).scrollTop() >= scrollDistance) {
+        if ($(window).scrollTop() >= scrollDistance) {
              animatePoints();
          }
      });
-
- });
+});
+ 
